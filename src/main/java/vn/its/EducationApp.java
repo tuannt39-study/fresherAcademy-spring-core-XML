12440d1e8c83d49f351763a352914bfe87f1c130
tuannt39 <tuannt39@gmail.com>
2018-03-10 20:08:42 +0700

    bean demo

--- src/main/java/vn/its/EducationApp.java
+++ src/main/java/vn/its/EducationApp.java
@@ -2,6 +2,7 @@ package vn.its;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import vn.its.service.StudentService;
 
 public class EducationApp {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class EducationApp {
 //        Student student = studentList.get(0);
 //        System.out.println(student);
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.xml");
-
+        StudentService service =  applicationContext.getBean("studentService", StudentService.class);
+        System.out.println(service.getAllStudent().size());
     }
 }
