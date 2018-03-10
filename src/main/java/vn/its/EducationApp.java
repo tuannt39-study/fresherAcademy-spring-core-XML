ea0b52ae5c62b60ac5aa8859fa2d4076cac536ed
tuannt39 <tuannt39@gmail.com>
2018-03-10 19:54:18 +0700

    xml configuration

--- src/main/java/vn/its/EducationApp.java
+++ src/main/java/vn/its/EducationApp.java
@@ -1,17 +1,16 @@
 package vn.its;
 
-import vn.its.model.Student;
-import vn.its.service.StudentService;
-import vn.its.service.StudentServiceImpl;
-
-import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class EducationApp {
     public static void main(String[] args) {
-        StudentService studentService = new StudentServiceImpl();
-        List<Student> studentList = studentService.getAllStudent();
-        System.out.println(studentList.size());
-        Student student = studentList.get(0);
-        System.out.println(student);
+//        StudentService studentService = new StudentServiceImpl();
+//        List<Student> studentList = studentService.getAllStudent();
+//        System.out.println(studentList.size());
+//        Student student = studentList.get(0);
+//        System.out.println(student);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.xml");
+
     }
 }
